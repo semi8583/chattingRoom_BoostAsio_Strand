af2175b378283fd92ac518d68996bf368791a027
semi8583 <100404914+semi8583@users.noreply.github.com>
2023-01-03 15:11:58 +0900

    Update logger.h

--- Server/Server/logger.h
+++ Server/Server/logger.h
@@ -1,15 +1,14 @@
 #include <iomanip>
 #include <fstream>
 #include <iostream>
-using namespace std;
 
 class ostreamFork           // Write same data to two ostreams
 {
 public:
-    ostream& os1;
-    ostream& os2;
+    std::ostream& os1;
+    std::ostream& os2;
 
-    ostreamFork(ostream& os_one, ostream& os_two);
+    ostreamFork(std::ostream& os_one, std::ostream& os_two);
 
     template <class Data>
     friend ostreamFork& operator<<(ostreamFork& osf, Data d)
@@ -20,7 +19,7 @@ public:
     }
     
     // For manipulators: endl, flush
-    friend ostreamFork& operator<<(ostreamFork& osf, ostream& (*f)(ostream&))
+    friend ostreamFork& operator<<(ostreamFork& osf, std::ostream& (*f)(std::ostream&))
     {
         osf.os1 << f;
         osf.os2 << f;
@@ -29,7 +28,7 @@ public:
 
     // For setw() , ...
     template<class ManipData>
-    friend ostreamFork& operator<<(ostreamFork& osf, ostream& (*f)(ostream&, ManipData))
+    friend ostreamFork& operator<<(ostreamFork& osf, std::ostream& (*f)(std::ostream&, ManipData))
     {
         osf.os1 << f;
         osf.os2 << f;
