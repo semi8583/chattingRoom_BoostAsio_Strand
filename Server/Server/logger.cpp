6483fbaf971a3f4b965cfee84848daaba2e4603e
semi8583 <100404914+semi8583@users.noreply.github.com>
2023-01-03 15:12:15 +0900

    Update logger.cpp

--- Server/Server/logger.cpp
+++ Server/Server/logger.cpp
@@ -1,7 +1,7 @@
 #include "logger.h"
 
 
-ostreamFork::ostreamFork(ostream& os_one, ostream& os_two)
+ostreamFork::ostreamFork(std::ostream& os_one, std::ostream& os_two)
 	: os1(os_one),
 	os2(os_two)
 {}
